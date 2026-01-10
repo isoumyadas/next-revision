4eb02e9a26a82f98f8979b017e13c7dd12797b64
isoumyadas <sammy8das@gmail.com>
2026-01-10 23:29:19 +0000

    feat: created github and discord oauth client

---
+++ auth/core/oauth/github.ts
@@ -0,0 +1,29 @@
+import z from "zod";
+import { OAuthClient } from "./base";
+
+export function createGithubOAuthClient() {
+  return new OAuthClient({
+    provider: "github",
+    clientId: process.env.GITHUB_CLIENT_ID!,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    scopes: ["user:email", "read: user"],
+    urls: {
+      auth: "https://github.com/login/oauth/authorize",
+      token: "https://github.com/login/oauth/access_token ",
+      user: "https://api.github.com/user",
+    },
+    userInfo: {
+      schema: z.object({
+        id: z.string(),
+        name: z.string().nullable(),
+        login: z.string(),
+        email: z.string().email(),
+      }),
+      parser: (user) => ({
+        id: user.id.toString(),
+        name: user.name ?? user.email,
+        email: user.email,
+      }),
+    },
+  });
+}
